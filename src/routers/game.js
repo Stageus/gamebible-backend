3398a33ebe4cc6654c05c8d0d6b09721cef1ccab
k1ju <gksrlwn98@naver.com>
2024-03-07 20:31:24 +0900

    Feat: 위키생성요청API 완성(pg, 토큰미들웨어 추가예정)

--- src/routers/game.js
+++ src/routers/game.js
@@ -1,6 +1,21 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
 //위키생성요청
-router.post('/')
+router.post('/request', async (req, res, next) => {
+    const { title } = req.body;
+    // 미들웨어에 authorization 반영
+
+    try {
+        await Pool.query(
+            `INSERT INTO request(user_idx, title) 
+                        VALUES ( $1 ,$2 ) `,
+            [useridx, title]
+        );
+
+        res.status(200).send();
+    } catch (e) {
+        next(e);
+    }
+});
 
 module.exports = router;
