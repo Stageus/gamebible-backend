b1ea0c647d438388f11224e4e3c7b83d281bafcc
k1ju <gksrlwn98@naver.com>
2024-03-07 19:20:29 +0900

    Chore: 라우터파일 생성, 라우터경로설정

--- server.js
+++ server.js
@@ -7,15 +7,17 @@ app.use('/', () => {
     res.send('Hello World');
 });
 
-const accountApi = require("./src/routers/account");
+const accountApi = require('./src/routers/account');
+const gameApi = require('./src/routers/game');
+const postApi = require('./src/routers/post');
+const commentApi = require('./src/routers/comment');
+const adminApi = require('./src/routers/adminApi');
 
 app.use('/', accountApi);
-
-
-
-
-
-
+app.use('/game', gameApi);
+app.use('/post', postApi);
+app.use('/comment', commentApi);
+app.use('/admin', adminApi);
 
 
 app.listen(process.env.HTTP_PORT);
--- src/routers/game.js
+++ src/routers/game.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
 
+//위키생성요청
+router.post('/')
 
 module.exports = router;
