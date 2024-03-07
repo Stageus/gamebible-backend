2a0d23c606d5ecf767aa6a50bd6deec578f8785d
k1ju <gksrlwn98@naver.com>
2024-03-07 20:45:57 +0900

    FIX: 위키생성요청API 수정

--- src/routers/game.js
+++ src/routers/game.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+// pg반영
 
 //위키생성요청
 router.post('/request', async (req, res, next) => {
     const { title } = req.body;
+    const { userIdx } = req.user;
     // 미들웨어에 authorization 반영
-
     try {
         await Pool.query(
             `INSERT INTO request(user_idx, title) 
@@ -17,5 +18,15 @@ router.post('/request', async (req, res, next) => {
         next(e);
     }
 });
+
+//위키목록불러오기
+router.get('/', async(req, res, next) => {
+    const lastIdx = req.query
+    try{
+
+    } catch (e) {
+
+    }
+})
 
 module.exports = router;
