fbabb1b0c89a214f7bb9abeb1dbd6c24ac2aa377
Tae Eun Jeong <147980144+nundung@users.noreply.github.com>
2024-03-10 16:52:19 +0900

    Feat: 게시글 쓰기 api 생성

--- src/routers/post.js
+++ src/routers/post.js
@@ -2,6 +2,34 @@
 const router = require('express').Router();
 
 //Apis
+
+//게시글 쓰기
+//이 api는 프론트와 상의 후 수정하기로..
+router.post('/', async (req, res, next) => {
+    const { title, content } = req.body;
+    const gameIdx = req.query.game_idx;
+    try {
+        const userIdx = req.decoded.userIdx;
+        const sql = `
+        INSERT INTO 
+            post(
+                user_idx,
+                game_idx,
+                title,
+                content
+                )
+            VALUE
+                ($1, $2, $3, $4)
+            RETURNING
+                idx`;
+        const values = [userIdx, gameIdx, title, content];
+        const result = await pool.query(sql, values);
+        res.status(201).send();
+    } catch (err) {
+        next(err);
+    }
+});
+
 //게시판 보기 (게시글 목록보기)
 router.get('/', async (req, res, next) => {
     const result = { data: null };
