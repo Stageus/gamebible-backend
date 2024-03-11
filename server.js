a5651892863527ee772015fa8fae2fe4fdd0e584
k1ju <gksrlwn98@naver.com>
2024-03-11 14:20:48 +0900

    Fix: server.js 오류 수정

--- server.js
+++ server.js
@@ -3,21 +3,18 @@ require('dotenv').config();
 
 const app = express();
 
-app.use('/', () => {
-    res.send('Hello World');
+// const accountApi = require('./src/routers/account');
+// const gameApi = require('./src/routers/game');
+// const postApi = require('./src/routers/post');
+// const commentApi = require('./src/routers/comment');
+// const adminApi = require('./src/routers/admin');
+
+// app.use('/account', accountApi);
+// app.use('/game', gameApi);
+// app.use('/post', postApi);
+// app.use('/comment', commentApi);
+// app.use('/admin', adminApi);
+
+app.listen(process.env.HTTP_PORT, () => {
+    console.log(`${process.env.HTTP_PORT}번 포트번호 서버실행`);
 });
-
-const accountApi = require('./src/routers/account');
-const gameApi = require('./src/routers/game');
-const postApi = require('./src/routers/post');
-const commentApi = require('./src/routers/comment');
-const adminApi = require('./src/routers/adminApi');
-
-app.use('/', accountApi);
-app.use('/game', gameApi);
-app.use('/post', postApi);
-app.use('/comment', commentApi);
-app.use('/admin', adminApi);
-
-
-app.listen(process.env.HTTP_PORT);
