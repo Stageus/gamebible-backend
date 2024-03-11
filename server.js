add0d40751a080672849b5b0ebbe2b00769f7b14
k1ju <gksrlwn98@naver.com>
2024-03-11 16:15:17 +0900

    Fix: server.js 주석해제

--- server.js
+++ server.js
@@ -3,17 +3,28 @@ require('dotenv').config();
 
 const app = express();
 
-// const accountApi = require('./src/routers/account');
-// const gameApi = require('./src/routers/game');
-// const postApi = require('./src/routers/post');
-// const commentApi = require('./src/routers/comment');
-// const adminApi = require('./src/routers/admin');
+app.use(express.json());
 
-// app.use('/account', accountApi);
-// app.use('/game', gameApi);
-// app.use('/post', postApi);
-// app.use('/comment', commentApi);
-// app.use('/admin', adminApi);
+const accountApi = require('./src/routers/account');
+const gameApi = require('./src/routers/game');
+const postApi = require('./src/routers/post');
+const commentApi = require('./src/routers/comment');
+const adminApi = require('./src/routers/admin');
+
+app.use('/account', accountApi);
+app.use('/game', gameApi);
+app.use('/post', postApi);
+app.use('/comment', commentApi);
+app.use('/admin', adminApi);
+
+app.use((req, res, next) => {
+    next({ status: 404, message: 'API 없음' });
+});
+
+app.use((err, req, res, next) => {
+    if (err.status) err.status = 500;
+    res.status(err.status).send();
+});
 
 app.listen(process.env.HTTP_PORT, () => {
     console.log(`${process.env.HTTP_PORT}번 포트번호 서버실행`);
