fd14ec03d0467662dfe1aeb2ecbbc3a6e14b39b4
gown0930 <haeju0930@naver.com>
2024-03-13 14:37:39 +0900

    Feat: 회원가입 API 생성

--- src/routers/account.js
+++ src/routers/account.js
@@ -12,4 +12,19 @@ router.post('/login', async (req, res, next) => {
     }
 });
 
+// 회원가입
+router.post('/signup', async (req, res) => {
+    const { id, pw, nickname, email, isadmin } = req.body;
+
+    try {
+        // 회원가입 처리
+        const insertUserSql =
+            'INSERT INTO user (id, password, nickname, email, isadmin) VALUES ($1, $2, $3, $4, $5)';
+        await queryDatabase(insertUserSql, [id, pw, nickname, email, isadmin]);
+        return res.status(200).send();
+    } catch (e) {
+        next(e);
+    }
+});
+
 module.exports = router;
