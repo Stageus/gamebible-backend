b25cd6690bbe00b27f8d6b0e16c29dfe19c07f87
gown0930 <haeju0930@naver.com>
2024-03-13 14:35:38 +0900

    Refactor: 로그인 API 수정

--- src/routers/account.js
+++ src/routers/account.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+const { pool } = require('../config/postgres.js'); // './db'는 db.js 파일의 경로를
 
 // 로그인
 router.post('/login', async (req, res, next) => {
     const { id, pw } = req.body;
     try {
-        await Pool.query(`SELECT * FROM user WHERE id = $1 AND password = $2`, [id, pw]);
+        await pool.query(`SELECT * FROM user WHERE id = $1 AND password = $2`, [id, pw]);
         res.status(200).send();
     } catch (e) {
         next(e);
