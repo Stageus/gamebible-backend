a029ba76ae373777aaa88fc044e1bbe4aa9660c2
k1ju <gksrlwn98@naver.com>
2024-03-13 15:26:47 +0900

    Fix: server.js 주석해제

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const app = express();
 
 app.use(express.json());
 
-// const accountApi = require('./src/routers/account');
+const accountApi = require('./src/routers/account');
 const gameApi = require('./src/routers/game');
-// const postApi = require('./src/routers/post');
-// const commentApi = require('./src/routers/comment');
-// const adminApi = require('./src/routers/admin');
+const postApi = require('./src/routers/post');
+const commentApi = require('./src/routers/comment');
+const adminApi = require('./src/routers/admin');
 
-// app.use('/account', accountApi);
+app.use('/account', accountApi);
 app.use('/game', gameApi);
-// app.use('/post', postApi);
-// app.use('/comment', commentApi);
-// app.use('/admin', adminApi);
+app.use('/post', postApi);
+app.use('/comment', commentApi);
+app.use('/admin', adminApi);
 
 app.use((req, res, next) => {
     next({ status: 404, message: 'API 없음' });
