ade7102538611d01bd9d393f08744e67f39c7fe7
gown0930 <haeju0930@naver.com>
2024-03-13 15:04:25 +0900

    Refactor: 로그인 API, 회원가입 API 경로명 수정

--- src/routers/account.js
+++ src/routers/account.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { pool } = require('../config/postgres.js'); // './db'는 db.js 파일의 경로를
 
 // 로그인
-router.post('/login', async (req, res, next) => {
+router.post('/auth', async (req, res, next) => {
     const { id, pw } = req.body;
     try {
         await pool.query(`SELECT * FROM user WHERE id = $1 AND password = $2`, [id, pw]);
@@ -13,15 +13,15 @@ router.post('/login', async (req, res, next) => {
 });
 
 // 회원가입
-router.post('/signup', async (req, res) => {
-    const { id, pw, nickname, email, isadmin } = req.body;
+router.post('/', async (req, res, next) => {
+    const { id, pw, pw_same, nickname, email, isadmin } = req.body;
 
     try {
         // 회원가입 처리
         const insertUserSql =
             'INSERT INTO user (id, password, nickname, email, isadmin) VALUES ($1, $2, $3, $4, $5)';
-        await queryDatabase(insertUserSql, [id, pw, nickname, email, isadmin]);
-        return res.status(200).send();
+        await pool.query(insertUserSql, [id, pw, nickname, email, isadmin]);
+        return res.status(200).send('회원가입 성공');
     } catch (e) {
         next(e);
     }
