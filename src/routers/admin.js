f8304fadb4c1d335f85291e7aa3cbcf6c195ba9f
k1ju <gksrlwn98@naver.com>
2024-03-18 13:05:06 +0900

    Feature/admin/game (#18)
    * Feat: 게임생성 API 생성
    
    * Fix: 게임생성API에 checkLogin 미들웨어 반영

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -1,3 +1,25 @@
 const router = require('express').Router();
+const { pool } = require('../config/posgres');
+const checkLogin = require('../modules/checkLogin');
+
+// 위키(게임) 생성
+// admin체크 미들웨어 추가
+router.post('/game', checkLogin, async (req, res, next) => {
+    const { title } = req.body;
+    const userIdx = req.body.useridx;
+    try {
+        const sql = `
+        INSERT INTO 
+            game(title, user_idx)
+        VALUES
+            ( $1, $2 )`;
+        const values = [title, userIdx];
+        await pool.query(sql, values);
+
+        res.status(201).send();
+    } catch (e) {
+        next(e);
+    }
+});
 
 module.exports = router;
