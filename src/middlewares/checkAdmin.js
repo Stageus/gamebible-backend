0f5e3980cf3b16c1e3573adf50d2e8ed07484534
k1ju <gksrlwn98@naver.com>
2024-03-18 15:26:00 +0900

    Test: checkAdmin 테스트코드작성

--- src/middlewares/checkAdmin.js
+++ src/middlewares/checkAdmin.js
@@ -2,16 +2,20 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const checkAdmin = async (req, res, next) => {
-    const { token } = req.headers;
+    console.log(req.headers);
+    console.log(req.headers.authorization);
+    const { authorization } = req.headers;
+
+    console.log(authorization);
 
     try {
-        if (!token) {
+        if (!authorization) {
             const error = new Error('no token');
             error.status = 401;
             throw error;
         }
-        req.decoded = jwt.verify(token, process.env.SECRET_KEY);
-        const isAdmin = req.decoded.isAdmin;
+        req.decoded = jwt.verify(authorization, process.env.SECRET_KEY);
+        const isAdmin = req.decoded.isadmin;
 
         if (isAdmin != true) {
             const error = new Error('no admin');
