2784501e406591ba16ba5732fa524a5198d9717a
k1ju <gksrlwn98@naver.com>
2024-03-18 12:59:41 +0900

    Fix: 게임생성API에 checkLogin 미들웨어 반영

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const { pool } = require('../config/posgres');
+const checkLogin = require('../modules/checkLogin');
 
 // 위키(게임) 생성
 // admin체크 미들웨어 추가
-router.post('/game', async (req, res, next) => {
+router.post('/game', checkLogin, async (req, res, next) => {
     const { title } = req.body;
     const userIdx = req.body.useridx;
     try {
