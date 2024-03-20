cd9a41df98d57627308b3c9b5c7a9c0cd4d9b98b
Tae Eun Jeong <147980144+nundung@users.noreply.github.com>
2024-03-20 17:44:59 +0900

    Feat: 댓글 쓰기 API 생성

--- src/routers/comment.js
+++ src/routers/comment.js
@@ -1,3 +1,31 @@
+//Import
 const router = require('express').Router();
+const { pool } = require('../config/postgres');
+const checkLogin = require('../middlewares/checkLogin');
+
+//Apis
+
+//댓글 쓰기
+router.post('/', checkLogin, async (req, res, next) => {
+    const content = req.body.content;
+    const postIdx = req.query.postidx;
+    try {
+        const userIdx = req.decoded.userIdx;
+        const sql = `
+            INSERT INTO
+                comment(
+                    user_idx,
+                    post_idx,
+                    content
+                )
+            VALUES
+                ($1, $2, $3)`;
+        const values = [userIdx, postIdx, content];
+        await pool.query(sql, values);
+        res.status(201).send();
+    } catch (err) {
+        next(err);
+    }
+});
 
 module.exports = router;
