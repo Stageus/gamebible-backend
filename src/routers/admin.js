c1e044bbc1d24a18b5e6aefb61703030c40e5108
k1ju <gksrlwn98@naver.com>
2024-03-21 15:37:49 +0900

    Feature/admin/game (#33)
    * Fix: 게임생성API 썸네일,배너 기본이미지 반영
    
    * Fix: 게임생성API isAdmin 변수삭제

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -7,7 +7,7 @@ const { uploadS3 } = require('../middlewares/upload');
 // 게임 생성
 router.post('/game', checkLogin, checkAdmin, async (req, res, next) => {
     const { requestIdx } = req.body;
-    const { userIdx, isAdmin } = req.decoded;
+    const { userIdx } = req.decoded;
     try {
         const updateRequestSQL = `
                             UPDATE
@@ -31,12 +31,39 @@ router.post('/game', checkLogin, checkAdmin, async (req, res, next) => {
         const title = selectResult.title;
 
         const insertGameSQL = `
-        INSERT INTO 
+        INSERT INTO
             game(title, user_idx)
         VALUES
             ( $1, $2 )`;
         const insertGamevalues = [title, userIdx];
         await pool.query(insertGameSQL, insertGamevalues);
+
+        const selectLatestGameSQL = `
+                                    SELECT 
+                                        idx
+                                    FROM
+                                        game
+                                    ORDER BY 
+                                        idx DESC
+                                    limit 1`;
+        const selectLatestGameResult = await pool.query(selectLatestGameSQL);
+        const latestGameIdx = selectLatestGameResult.rows[0].idx;
+
+        const insertThumnailSQL = `
+                                INSERT INTO
+                                    game_img_thumnail(game_idx)
+                                VALUES ( $1 )
+                                 `;
+        const insertThumnailValues = [latestGameIdx];
+        await pool.query(insertThumnailSQL, insertThumnailValues);
+
+        const insertBannerSQL = `
+                                INSERT INTO
+                                    game_img_banner(game_idx)
+                                VALUES ( $1 )
+                                 `;
+        const insertBannerValues = [latestGameIdx];
+        await pool.query(insertBannerSQL, insertBannerValues);
 
         res.status(201).send();
     } catch (e) {
