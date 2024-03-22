4cb5f5ad14d69eac558932c5aab1b403123e5494
k1ju <gksrlwn98@naver.com>
2024-03-22 13:07:37 +0900

    Test: generateNotification 모듈 테스트코드 생성

---
+++ src/modules/generateNotification.js
@@ -0,0 +1,12 @@
+const { pool } = require('../config/postgres');
+
+const generateNotification = async (type, userIdx, gameIdx, postIdx) => {
+    const insertNotificationSQL = `
+                                INSERT INTO
+                                    notification (type, user_game, game_idx, post_idx)
+                                VALUES( $1, $2, $3, $4 )`;
+    const insertNotificationValues = [type, userIdx, gameIdx, postIdx];
+    await pool.query(insertNotificationSQL, insertNotificationValues);
+};
+
+module.exports = { generateNotification };
