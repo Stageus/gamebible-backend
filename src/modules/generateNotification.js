787222ab29d9b13df45fa69f3afd2b564d3c1353
k1ju <gksrlwn98@naver.com>
2024-03-23 18:31:11 +0900

    Fix: 게임수정하기API 유저들알림기능 추가

--- src/modules/generateNotification.js
+++ src/modules/generateNotification.js
@@ -1,12 +1,10 @@
-const { pool } = require('../config/postgres');
-
-const generateNotification = async (type, userIdx, gameIdx, postIdx) => {
+const generateNotification = async (poolClient, type, userIdx, gameIdx, postIdx) => {
     const insertNotificationSQL = `
                                 INSERT INTO
                                     notification (type, user_idx, game_idx, post_idx)
                                 VALUES( $1, $2, $3, $4 )`;
     const insertNotificationValues = [type, userIdx, gameIdx, postIdx];
-    await pool.query(insertNotificationSQL, insertNotificationValues);
+    await poolClient.query(insertNotificationSQL, insertNotificationValues);
 };
 
 module.exports = { generateNotification };
