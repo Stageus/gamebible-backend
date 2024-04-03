81693a7c8b6c1b3b7142d179bf5f824716fe38a0
k1ju <gksrlwn98@naver.com>
2024-04-03 20:04:21 +0900

    Refactor: 임시위키생성API 콘솔문제거

--- src/routers/game.js
+++ src/routers/game.js
@@ -369,7 +369,6 @@ router.post('/:gameidx/wiki', checkLogin, async (req, res, next) => {
         );
         const history = queryResult.rows[0];
         const historyIdx = history.idx;
-        console.log('historyIdx', historyIdx);
         res.status(201).send({ data: historyIdx });
     } catch (e) {
         next(e);
