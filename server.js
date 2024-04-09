ee746b488a36787bf6dc089f879c1a3a0e2898c4
k1ju <gksrlwn98@naver.com>
2024-04-09 17:10:59 +0900

    Chore: 서버.js limit추가

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: false }));
 
 const accountApi = require('./src/routers/account');
 const gameApi = require('./src/routers/game');
