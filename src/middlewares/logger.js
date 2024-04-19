8ef2b6e71258d18c0b4b9d765aa5dd2df16cb0f0
Tae Eun Jeong <147980144+nundung@users.noreply.github.com>
2024-04-19 19:37:16 +0900

    Refactor: logger.js 문서 코드 리팩토링

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,9 +1,9 @@
 const { pool } = require('../config/postgres');
 
-const log = async (req, res, next) => {
+const logger = async (req, res, next) => {
     const logData = {
         ip: req.ip,
-        idx: req.decoded ? req.decoded.idx : 'unknown',
+        idx: req.decoded ? req.decoded.userIdx : null,
         url: req.originalUrl,
         method: req.method,
         requestedTimestamp: new Date(),
@@ -11,34 +11,38 @@ const log = async (req, res, next) => {
         status: null,
         stackTrace: null,
     };
+    console.log(req.decoded);
     res.on('finish', async () => {
         logData.respondedTimestamp = new Date();
         logData.status = res.locals.error ? res.locals.error.statusCode : res.statusCode;
         logData.stackTrace = res.locals.error ? res.locals.error.stackTrace : null;
-        logData.message = res.locals.error ? res.locals.error.message : null;
-
+        console.log(logData);
         try {
-            const client = await pool.connect();
-            await client.query('BEGIN');
-            const queryText = `
-                INSERT INTO logs(ip, idx, url, method, requested_timestamp, responded_timestamp, status, stack_trace, message)
-                VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
-            `;
-            const values = [
-                logData.ip,
-                logData.idx,
-                logData.url,
-                logData.method,
-                logData.requestedTimestamp,
-                logData.respondedTimestamp,
-                logData.status,
-                logData.stackTrace,
-                logData.message,
-            ];
-            await client.query(queryText, values);
-            await client.query('COMMIT');
-            client.release();
-            console.log('로그가 성공적으로 저장되었습니다.');
+            await pool.query(
+                `INSERT INTO 
+                    log(
+                        ip,
+                        user_idx,
+                        url,
+                        method,
+                        requested_timestamp,
+                        responded_timestamp,
+                        status,
+                        stack_trace
+                    )
+                VALUES
+                    ($1, $2, $3, $4, $5, $6, $7, $8)`,
+                [
+                    logData.ip,
+                    logData.idx,
+                    logData.url,
+                    logData.method,
+                    logData.requestedTimestamp,
+                    logData.respondedTimestamp,
+                    logData.status,
+                    logData.stackTrace,
+                ]
+            );
         } catch (err) {
             console.error('로그 저장 중 오류 발생:', err);
         }
@@ -46,4 +50,4 @@ const log = async (req, res, next) => {
     next();
 };
 
-module.exports = { log };
+module.exports = { logger };
