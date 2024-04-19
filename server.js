88485a3d6dc64c7f6459e80d8321e77e5571e7b0
k1ju <gksrlwn98@naver.com>
2024-04-19 19:38:13 +0900

    Feat: 로그기능생성

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { logger } = require('./src/middlewares/logger');
 require('dotenv').config();
 
 const app = express();
@@ -8,12 +9,15 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: false }));
 
+const logApi = require('./src/routers/log');
 const accountApi = require('./src/routers/account');
 const gameApi = require('./src/routers/game');
 const postApi = require('./src/routers/post');
 const commentApi = require('./src/routers/comment');
 const adminApi = require('./src/routers/admin');
 
+app.use(logger);
+app.use('/log', logApi);
 app.use('/account', accountApi);
 app.use('/game', gameApi);
 app.use('/post', postApi);
